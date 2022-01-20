ec6e1d13a8c0176b8a618009c8d57ade531563bc
colbyfayock <fayock@gmail.com>
2022-01-19 21:43:28 -0500

    Step 2: installing the Cloudinary SDK and configuring an account

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,3 +1,11 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 export default async function handler(req, res) {
   const data = JSON.parse(req.body);
   res.status(200).json(data);
